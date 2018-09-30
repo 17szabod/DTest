927533d93f69b24496f1c4c6ee4ac362a3bb435d
Daniel Szabo <daniszabo99@gmail.com>
2018-09-30 15:56:53 -0500

    Creates header file

---
+++ DTest.c
@@ -0,0 +1,166 @@
+//
+// Created by daniel on 9/28/18.
+//
+
+#include "DTest.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <python3.7m/Python.h>
+
+enum systemTypes {
+    Rhino = 0, OpenCasCade = 1, OpenSCAD = 2
+};
+
+Template readTemplate(char *filename, char *testName) {
+    FILE *fp = fopen(filename, "r");
+    Template out;
+    int ifp;
+    ifp = fscanf(fp, "%f\n%i\n%i\n%i\n%f\n%i\n%f\n%i\n%s", &out.algorithmPrecision, &out.connected, &out.convex,
+                 &out.manifold, &out.minimumFeatureSize, &out.queries, &out.systemTolerance, &out.system, out.model);
+    if (ifp >= 0) {
+        fprintf(stderr, "Failed to read file with name: %s", filename);
+        fclose(fp);
+        exit(1);
+    }
+    fclose(fp);
+    return out;
+}
+
+Properties startConfigureScript(Template template) {
+    Properties prop1;
+    // TODO: Make http requests to find the properties of the two models
+    if (template.system == OpenCasCade) {
+        PyObject *pName, *pModule, *pFunc;
+        PyObject *pArgs, *pValue, *pArg1, *pArg2, *pArg3;
+        Py_Initialize();
+        pName = PyUnicode_DecodeFSDefault("py_interface.py");
+        pModule = PyImport_Import(pName);
+        Py_DECREF(pName);
+        if (pModule != NULL) {
+            pFunc = PyObject_GetAttrString(pModule, "occ_configure");
+            /* pFunc is a new reference */
+
+            if (pFunc && PyCallable_Check(pFunc)) {
+                pArgs = PyTuple_New(3);
+                // The arguments will be: Sys_eps, alg_eps, and some access to the shape (filename?)
+                pArg1 = PyLong_FromDouble(template.systemTolerance);
+                pArg2 = PyLong_FromDouble(template.algorithmPrecision);
+                pArg3 = PyUnicode_DecodeFSDefault(template.model);
+                PyTuple_SetItem(pArgs, 0, pArg1);
+                PyTuple_SetItem(pArgs, 1, pArg2);
+                PyTuple_SetItem(pArgs, 2, pArg3);
+                pValue = PyObject_CallObject(pFunc, pArgs);
+                Py_DECREF(pArgs);
+                if (pValue != NULL) {
+                    PyObject *pHaussDist, *pSurfAr, *pVol, *pProx;
+                    PyObject *pSize;
+                    pHaussDist = PyTuple_GetItem(pValue, 0);
+                    pSurfAr = PyTuple_GetItem(pValue, 1);
+                    pVol = PyTuple_GetItem(pValue, 2);
+                    pProx = PyTuple_GetItem(pValue, 3);
+                    prop1.hausdorffDistance = (float) PyLong_AsDouble(pHaussDist);
+                    prop1.surfaceArea = (float) PyLong_AsDouble(pSurfAr);
+                    prop1.volume = (float) PyLong_AsDouble(pVol);
+                    pSize = PyLong_FromSsize_t(PyDict_Size(pProx));
+                    long size = PyLong_AsLong(pSize);
+                    double **arr = malloc(size * sizeof(double*));
+                    for (int i = 0; i < size; i++) {
+                        arr[i] = malloc(4 * sizeof(double));
+                    }
+                    PyObject *key, *value;
+                    PyObject *xVal, *yVal, *zVal;
+                    double x, y, z;
+                    int i = 0;
+                    Py_ssize_t pos = 0;
+                    while (PyDict_Next(pProx, &pos, &key, &value)) {
+                        xVal = PyTuple_GetItem(value, 0);
+                        yVal = PyTuple_GetItem(value, 1);
+                        zVal = PyTuple_GetItem(value, 2);
+                        x = PyLong_AsDouble(xVal);
+                        y = PyLong_AsDouble(yVal);
+                        z = PyLong_AsDouble(zVal);
+                        if (x == -1 && PyErr_Occurred()) {
+                            Py_DECREF(pArg1); Py_DECREF(pArg2); Py_DECREF(pArg3); Py_DECREF(pHaussDist); Py_DECREF(pos);
+                            Py_DECREF(pProx); Py_DECREF(pSize); Py_DECREF(pSurfAr); Py_DECREF(pVol);
+                            Py_DECREF(pModule);
+                            PyErr_Print();
+                            fprintf(stderr, "Failed to read value %i in the proxy model.\n", i);
+                            exit(1);
+                        }
+                        arr[i][0] = x;
+                        arr[i][1] = y;
+                        arr[i][2] = z;
+                        arr[i][3] = PyLong_AsDouble(value);
+                        i++;
+                    }
+                    prop1.proxyModel = arr;
+                    Py_DECREF(pArg1); Py_DECREF(pArg2); Py_DECREF(pArg3); Py_DECREF(pHaussDist); Py_DECREF(pos);
+                    Py_DECREF(pProx); Py_DECREF(pSize); Py_DECREF(pSurfAr); Py_DECREF(pVol);
+                    Py_XDECREF(pFunc);
+                    Py_DECREF(pModule);
+                    return prop1;
+                } else {
+                    Py_DECREF(pFunc);
+                    Py_DECREF(pModule);
+                    PyErr_Print();
+                    fprintf(stderr, "Call failed\n");
+                    exit(1);
+                }
+            } else {
+                if (PyErr_Occurred())
+                    PyErr_Print();
+                fprintf(stderr, "Cannot find function \"%s\"\n", "occ_configure");
+            }
+        }
+        if (Py_FinalizeEx() < 0) {
+            fprintf(stderr, "Failed to close python connection\n");
+            exit(1);
+        }
+    }
+    else if (template.system == Rhino) {
+
+    }
+    fprintf(stderr, "System not recognized, aborting\n");
+    exit(1);
+}
+
+int setTolerance(float tol) {
+    tolerance = tol;
+    if (tolerance == tol)
+        return 0;
+    else
+        return 1;
+}
+
+float getTolerance() {
+    return tolerance;
+}
+
+int performEvaluation(Properties p1, Properties p2) {
+    // TODO: Call the evaluation scripts
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        printf("Usage: ./DTest <TemplateFile1> <TemplateFile2> <TestName>\n");
+        exit(1);
+    }
+    char *file1 = argv[1];
+    char *file2 = argv[2];
+    char *test_name = argv[3];
+
+    Template temp1 = readTemplate(file1, test_name);
+    Template temp2 = readTemplate(file2, test_name);
+
+    Properties prop1 = startConfigureScript(temp1);
+    Properties prop2 = startConfigureScript(temp2);
+
+    int eval = performEvaluation(prop1, prop2);
+    if (eval == 0)
+        return 0;
+    else {
+        printf("Failed to perform evaluation\n");
+        exit(1);
+    }
+};
---
+++ DTest.h
@@ -0,0 +1,42 @@
+//
+// Created by daniel on 9/27/18.
+//
+
+#ifndef DTEST_DTEST_H
+#define DTEST_DTEST_H
+
+typedef struct {
+    int system;
+    float systemTolerance;
+    float algorithmPrecision;
+    int queries; // The queries supported will be represented by a bitmask-- we will define a set of all possible
+    // queries (less than 32), and the bitmask will represent a subset that is supported.
+    int manifold;
+    int connected;
+    int convex;
+    float minimumFeatureSize;
+    char *model; // A way to access the model, whether it be a filename or a unique id in a database (filename for now)
+} Template;
+
+typedef struct {
+    float hausdorffDistance;
+    float surfaceArea;
+    float volume;
+    double **proxyModel;  // A 2D, n by 4 array representing a union of balls -- Should we put this in Properties???
+    // NOTE: proxyModel, because of its size, is on the heap, so free it after use
+} Properties;
+
+float tolerance;
+
+Template readTemplate(char* filename, char* testName);
+
+int setTolerance(float tol);
+
+float getTolerance();
+
+Properties startConfigureScript(Template template);
+
+int performEvaluation(Properties p1, Properties p2);
+
+
+#endif //DTEST_DTEST_H
