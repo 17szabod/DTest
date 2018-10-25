99c96a135fa8dc7a7191ab805ea4bd1a78b74810
Daniel Szabo <daniszabo99@gmail.com>
2018-10-25 17:09:58 -0500

    Delete hmm.py

--- hmm.py
+++
@@ -1,7 +0,0 @@
-import sys
-from distutils.core import setup, Extension
-for p in sys.path:
-    print(p)
-setup(name='hmm', version='1.0', \
-      ext_modules=[Extension('hmming', ['DTest.c'])])
-print(sys.exec_prefix)
